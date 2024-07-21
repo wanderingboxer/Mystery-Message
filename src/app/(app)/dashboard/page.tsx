9d54f29c540fe027f2c1a0f290bfefc844290cef
Aditya Saxena <aditya.saxena4052@gmail.com>
2024-07-21 19:25:02 +0530

    error fixed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -181,7 +181,7 @@ function UserDashboard() {
         {messages.length > 0 ? (
           messages.map((message, index) => (
             <MessageCard
-              key={message._id}
+              // key={message._id}
               message={message}
               onMessageDelete={handleDeleteMessage}
             />
